bcea896ea615f6f49cfe113b0f1bd0c986cc9962
roboevt <70998278+roboevt@users.noreply.github.com>
2021-10-23 19:13:32 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
-import socket
 import time
 import os
-
 from dotenv import load_dotenv
 from fastapi import FastAPI
 import requests
+
 load_dotenv()
 app = FastAPI()
-#ec2Ip = "http://3.21.169.95"  # DBF ec2 ip, Put into .env
-#ec2Ip = "http://3.144.155.72:8000/printerip/" # Personal ec2 ip for testing
+
 ec2Ip = os.getenv('botIp')
-load_dotenv()
-GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
 sleepTime = int(os.getenv('sleepTime'))
 
 while True:
